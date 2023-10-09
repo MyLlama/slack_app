45a59f36a10539112c4fcc898cee24d86488bd4a
Shivam Sherje <shivamsherje@Shivams-MacBook-Air.local>
2023-10-09 16:05:55 +0530

    Update : Home view discription : Created new home view and daily checkin button

--- listeners/events/app-home-opened.js
+++ listeners/events/app-home-opened.js
@@ -1,3 +1,5 @@
+const view = require('../../user-interface/app-home/unauthorised-user-view');
+
 const appHomeOpenedCallback = async ({ client, event }) => {
   // Ignore the `app_home_opened` event for anything but the Home tab
   if (event.tab !== 'home') return;
@@ -5,25 +7,7 @@ const appHomeOpenedCallback = async ({ client, event }) => {
   try {
     await client.views.publish({
       user_id: event.user,
-      view: {
-        type: 'home',
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: `*Welcome home, <@${event.user}> :house:*`,
-            },
-          },
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.',
-            },
-          },
-        ],
-      },
+      view,
     });
   } catch (error) {
     console.error(error);
