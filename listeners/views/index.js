86bfadeb094f57b7d0248d2891813fa122d152c5
shivamsherje <sherjes49@gmail.com>
2023-10-16 15:27:22 +0530

    Improved : Made options dynamic and change the file name

--- listeners/views/index.js
+++ listeners/views/index.js
@@ -1,5 +1,5 @@
 const { sampleViewCallback } = require('./sample-view');
-const { postDailyCheckinFeedback } = require('./daily-checkins');
+const { postDailyCheckinFeedback } = require('./daily-checkin');
 
 module.exports.register = (app) => {
   app.view('sample_view_id', sampleViewCallback);
--- user-interface/models/daily-checkin-modal.js
+++ user-interface/models/daily-checkin-modal.js
@@ -2,65 +2,34 @@ const { getDailyCheckinQuestions } = require('../../services/strapi/index');
 
 async function getDailyCheckinModal(username) {
   const dailyCheckinQuestions = await getDailyCheckinQuestions();
-  const questions = dailyCheckinQuestions.map((dailyCheckinQuestion) => ({
-    type: 'input',
-    element: {
-      type: 'static_select',
-      placeholder: {
+  const questions = dailyCheckinQuestions.map((dailyCheckinQuestion) => {
+    const options = dailyCheckinQuestion.options.map((dailyCheckinOption) => ({
+      text: {
         type: 'plain_text',
-        text: 'I feel..',
+        text: dailyCheckinOption,
         emoji: true,
       },
-      options: [
-        {
-          text: {
-            type: 'plain_text',
-            text: dailyCheckinQuestion.options[0],
-            emoji: true,
-          },
-          value: '5',
+      value: dailyCheckinOption,
+    }));
+    return {
+      type: 'input',
+      element: {
+        type: 'static_select',
+        placeholder: {
+          type: 'plain_text',
+          text: 'I feel..',
+          emoji: true,
         },
-        {
-          text: {
-            type: 'plain_text',
-            text: dailyCheckinQuestion.options[1],
-            emoji: true,
-          },
-          value: '4',
-        },
-        {
-          text: {
-            type: 'plain_text',
-            text: dailyCheckinQuestion.options[2],
-            emoji: true,
-          },
-          value: '3',
-        },
-        {
-          text: {
-            type: 'plain_text',
-            text: dailyCheckinQuestion.options[3],
-            emoji: true,
-          },
-          value: '2',
-        },
-        {
-          text: {
-            type: 'plain_text',
-            text: dailyCheckinQuestion.options[4],
-            emoji: true,
-          },
-          value: '1',
-        },
-      ],
-      action_id: dailyCheckinQuestion.question,
-    },
-    label: {
-      type: 'plain_text',
-      text: dailyCheckinQuestion.question,
-      emoji: true,
-    },
-  }));
+        options,
+        action_id: dailyCheckinQuestion.question,
+      },
+      label: {
+        type: 'plain_text',
+        text: dailyCheckinQuestion.question,
+        emoji: true,
+      },
+    };
+  });
 
   return {
     title: {
@@ -88,4 +57,5 @@ async function getDailyCheckinModal(username) {
     callback_id: 'postDailyCheckin',
   };
 }
+
 module.exports = getDailyCheckinModal;
