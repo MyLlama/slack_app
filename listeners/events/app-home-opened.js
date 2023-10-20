1397f4952b381d0fae03fbc9a96eaac7bf3161fa
shivamsherje <sherjes49@gmail.com>
2023-10-20 11:00:31 +0530

    Added : Intro msg and Footer in slack app

--- listeners/events/app-home-opened.js
+++ listeners/events/app-home-opened.js
@@ -1,7 +1,11 @@
 const view = require('../../user-interface/app-home/unauthorised-user-view');
+const { appIntroCallback } = require('../messages/app-intro');
 
-const appHomeOpenedCallback = async ({ client, event }) => {
-  if (event.tab !== 'home') return;
+const appHomeOpenedCallback = async ({ client, event, say }) => {
+  if (event.tab == 'home' && !event.view) {
+    await appIntroCallback(say, event);
+  }
+  console.log(event.user);
   const homeView = await view.unauthorisedUserView();
   try {
     await client.views.publish({
