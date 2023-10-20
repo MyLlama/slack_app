4c5686a2960ae20aae928120d599aac8b15f1afc
Disha <dishatalreja1202@gmail.com>
2023-10-20 12:54:33 +0530

    Added: comment and improved logic for fetching welcome message

--- listeners/events/app-home-opened.js
+++ listeners/events/app-home-opened.js
@@ -2,8 +2,9 @@ const view = require('../../user-interface/app-home/unauthorised-user-view');
 const { getWelcomeMessage } = require('../../services/strapi/index');
 
 const appHomeOpenedCallback = async ({ client, event, say }) => {
-  const welcomeMessage = await getWelcomeMessage();
+// Send welcome message to the user when app home is opened for the first time.
   if (event.tab == 'home' && !event.view) {
+    const welcomeMessage = await getWelcomeMessage();
     say(`:wave: Hey <@${event.user}> \n \n ${welcomeMessage}`);
   }
 
